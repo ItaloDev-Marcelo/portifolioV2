8bdbad869e239e33bace73b452a234b0bb428be3
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-06-04 10:24:27 -0300

    reafactor: create a communStyle to use on my styleItem1 and styleItem2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,10 +9,11 @@ type navPropsType = {
 
 export const Navbar = ({ hundleNavbar, openBar }: navPropsType) => {
 
-  const styleItem1 = `relative        
+
+  const communStyle = `relative        
     pb-1
     md:my-3
-    my-10
+    my-5
     lg:my-0
     w-fit
     font-medium
@@ -41,50 +42,24 @@ export const Navbar = ({ hundleNavbar, openBar }: navPropsType) => {
     after:bg-blue-800
 
     after:transition-all
-    after:duration-300
+    after:duration-300`
+
+  const styleItem1 = `
+    ${communStyle}
     hover:after:w-[25%]
     hover:before:w-[75%]
     active:after:w-[25%]
-    active:before:w-[75%]`
+    active:before:w-[75%]
+    `
 
-  const styleItem2 = `relative       
-    pb-1
-    my-10
-    md:my-3
-    lg:my-0
-    w-fit
-    font-medium
-    text-2xl
-    lg:text-[1.2rem]
-    cursor-pointer
-
-
-    before:absolute
-    before:left-0
-    lg:before:left-0
-    before:bottom-0
-    lg:before:top-15
-    before:h-0.5
-    before:w-0
-    before:bg-gray-300
-
-    after:absolute
-    after:left-0
-    lg:after:left-5
-    after:bottom-0
-    lg:after:top-14.5
-    after:h-0.75
-    after:w-0
-    lg:pr-10
-    after:bg-blue-800
-
-    after:transition-all
-    after:duration-300
+  const styleItem2 = `
+    ${communStyle}
+    hover:after:w-[25%]
+    hover:before:w-[60%]
     active:after:w-[25%]
-    active:before:w-[60%]
-    lg:hover:after:w-[25%]
-    lg:hover:before:w-[60%]`
-
+    active:before:w-[75%]
+    `
+  
 
   const navList2 = [
     {
@@ -117,9 +92,9 @@ export const Navbar = ({ hundleNavbar, openBar }: navPropsType) => {
   return (
     <div className='flex flex-row justify-between items-center p-4 lg:py-6 lg:px-9 bg-white blue-200 border-b-2 border-gray-100 '>
 
-      <div className='ml-7'><a href='index.html'><img src={logo} alt='logo' className='w-15 md:w-17' /></a></div>
+    <a href='index.html' className='lg:ml-7'><img src={logo} alt='logo' className='w-15 md:w-17' /></a>
 
-      <div className='lg:hidden'><button onClick={hundleNavbar}>{!openBar ? <FiAlignJustify size='35' /> : <FiX size='35' />}</button></div>
+    <button onClick={hundleNavbar} className='lg:hidden '>{!openBar ? <FiAlignJustify size='35' /> : <FiX size='35' />}</button>
 
       <nav className={`${openBar ? 'flex' : 'hidden lg:flex'}  shadow-hidden lg:h-auto lg:w-auto w-screen h-full lg:relative lg:top-0  fixed left-0 top-18  bg-[#ececfb] lg:bg-transparent`}>
         <ul className='pt-16 lg:pt-0 px-8  box-border relative flex flex-col lg:flex-row'>
