8988f6c61a8bbb3ad07cf6900f5a9f8f3881a448
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-06-09 14:56:28 -0300

    feat: create Main page to use in my fullPage

---
+++ src/pages/Main.tsx
@@ -0,0 +1,24 @@
+import SkillsCards from '../components/SkillsCards';
+import { IconsData } from '../data/SkillsCardsData';
+
+const Main = () => {
+  return (
+    <main>
+       <section className='flex flex-col p-5 justify-center bg-[#feffff] content-center items-center shadow-sm'>
+             <div className='relative pb-3'>
+                 <h2 className='font-bold text-4xl my-10'>Skills</h2>
+                 <hr className='relative -top-8 w-14 left-4 border-b-2 border-blue-700 ' />
+             </div>
+          <div className='flex flex-row items-center justify-center  gap-3 xl:gap-5 flex-wrap'>
+             {
+            IconsData.map((item) => (
+               <SkillsCards iconTitle={item.title} skillsIcon={item.icon} color={item.color} />
+            ))
+           }
+          </div>
+        </section> 
+    </main>
+  )
+}
+
+export default Main
