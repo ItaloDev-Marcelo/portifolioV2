0bc1d131dc4af4f3a88ae92ad6e09caa7c5d0e7e
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-06-24 17:59:07 -0300

    refactor: add a link to my socialBtn in socialRow

--- src/components/SocialRow.tsx
+++ src/components/SocialRow.tsx
@@ -8,18 +8,38 @@ export const SocialRow = () => {
 
   return (
     <div className="flex flex-row mt-4  content-center">
-      <SocialBtn>
+   
+      <a  href="https://github.com/ItaloDev-Marcelo"
+  target="_blank"
+  rel="noopener noreferrer">
+    <SocialBtn>
         <FaGithub size='20'/>
       </SocialBtn>
-      <SocialBtn>
+  </a>
+
+      <a href="https://www.linkedin.com/in/italo-marcelo-06b130241"
+  target="_blank"
+  rel="noopener noreferrer">
+    <SocialBtn>
          <FaLinkedinIn size='20'/>
       </SocialBtn>
+  </a>
+      
+      <a href="https://wa.me/5573981339671?text=Olá,%20gostaria%20de%20falar%20com%20você.">
       <SocialBtn>
          <IoLogoWhatsapp size='20' />
       </SocialBtn>
-      <SocialBtn>
+      </a>
+
+      
+      <a href='mailto:devfrontend2028study@gmail.com' >
+        <SocialBtn>
           <MdEmail size='20'/>
       </SocialBtn>
+      </a>
+
+    
+
     </div>
   )
 }
